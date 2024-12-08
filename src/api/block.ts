1f1bf4c81e7d1c8fe4f8ba684174a7c6bb63c5ff
diogodsg <diogodasilvagouveia@gmail.com>
2024-12-08 15:22:20 -0300

    update url

--- src/api/block.ts
+++ src/api/block.ts
@@ -6,14 +6,14 @@ export class BlockchainService {
     formData.append("file", file);
 
     const res = await axios.post(
-      "http://blockchurna.tech/core/upload-file-block",
+      "http://core.blockchurna.tech/core/v1/upload-file-block",
       formData
     );
     return res.data;
   }
 
   static async getBlocks() {
-    const res = await axios.get("http://blockchurna.tech/core/blocks");
+    const res = await axios.get("http://core.blockchurna.tech/core/v1/blocks");
     return res.data;
   }
 
@@ -24,11 +24,14 @@ export class BlockchainService {
   ): Promise<{
     vote: { position: string; candidate: string; hash: string };
   }> {
-    const res = await axios.post("http://blockchurna.tech/core/verify-vote", {
-      voter_id: userId,
-      user_pin: userPin,
-      tse_pin: tsePin,
-    });
+    const res = await axios.post(
+      "http://core.blockchurna.tech/core/v1/verify-vote",
+      {
+        voter_id: userId,
+        user_pin: userPin,
+        tse_pin: tsePin,
+      }
+    );
     return res.data;
   }
 }
