aa491d69fcf032e5ca070bea33afbeed105042fb
diogodsg <diogodasilvagouveia@gmail.com>
2024-12-08 14:11:36 -0300

    REVOLUÇÃO ZA RODINU ZA STALIN

--- src/block-explorer/BlockExplorer.tsx
+++ src/block-explorer/BlockExplorer.tsx
@@ -56,6 +56,42 @@ export const BlockExplorer = () => {
               <Blocks blocks={blocks} />
             </div>
           </div>
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Download"
+            defaultChecked
+          />
+          <div role="tabpanel" className="tab-content p-6 ">
+            <div>
+              <h1 className="text-3xl font-bold my-4 text-slate-200">
+                Se torne um nó
+              </h1>
+              <span className="mb-8">
+                Junte-se a nós em uma rede de blockchain para transformar a
+                votação! Como um nó, você garantirá segurança, transparência e
+                integridade, tornando o processo eleitoral mais justo e
+                descentralizado. Faça parte dessa revolução pela confiança no
+                voto!
+              </span>
+              <div className="flex flex-col mt-8">
+                <a
+                  className="link"
+                  href="https://drive.google.com/uc?export=download&id=1J-Sk-ylAl7PeJMpslqbsGWr2H0YDuvOG"
+                >
+                  Aplicativo Windows
+                </a>
+                <a
+                  className="link"
+                  href="https://drive.google.com/uc?export=download&id=1QbvCM8gj2MKMgMA8MtQJce1912XAOzsp"
+                >
+                  Aplicativo Linux
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
